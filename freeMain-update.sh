5673424238457ba61eb25088d4f3a874ef5bcc90
niuzhihuan <zhihuan.niu@funi.com>
2018-06-15 17:52:39 +0800

    commit

--- freeMain-update.sh
+++ freeMain-update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "update-freeMind"
 cd /home/niu/Documents/ebook/freeMind
-git add */*
+git add ./
 git merge origin/master
 git commit -m "commit"
 git push -u origin master
--- 如何高效学习.mm
+++ 如何高效学习.mm
@@ -1,15 +1,6 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1523322568467" ID="ID_1144187435" MODIFIED="1523834248548" TEXT="&#x5982;&#x4f55;&#x9ad8;&#x6548;&#x5b66;&#x4e60;">
-<richcontent TYPE="NOTE"><html>
-  <head>
-    
-  </head>
-  <body>
-    &lt;html&gt; &lt;head&gt; &lt;/head&gt; &lt;body&gt; &lt;p&gt; &amp;#23398;&amp;#20064;&lt;content $ename=&quot;content&quot;&gt; &lt;/p&gt; &lt;/body&gt; &lt;/html&gt;
-  </body>
-</html>
-</richcontent>
+<node CREATED="1523322568467" ID="ID_1144187435" MODIFIED="1529055930331" TEXT="&#x5982;&#x4f55;&#x9ad8;&#x6548;&#x5b66;&#x4e60;">
 <node CREATED="1523327058625" ID="ID_78886488" MODIFIED="1523327109074" POSITION="right" TEXT="&#x7b2c;&#x4e00;&#x90e8;&#x5206; &#x6574;&#x4f53;&#x6027;&#x5b66;&#x4e60;&#x7b56;&#x7565;">
 <node CREATED="1523834872823" ID="ID_1277998929" MODIFIED="1523834887378" TEXT="&#x4ec0;&#x4e48;&#x662f;&#x6574;&#x4f53;&#x6027;&#x5b66;&#x4e60;">
 <node CREATED="1523834895497" ID="ID_1162738410" MODIFIED="1523834899960" TEXT="&#x5982;&#x4f55;&#x5b58;&#x50a8;&#x4fe1;&#x606f;"/>
