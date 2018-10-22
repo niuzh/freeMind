c73b2bcb9ff45e0fadab4a9785517e3e60586927
niuzhihuan <zhihuan.niu@funi.com>
2018-10-22 09:45:42 +0800

    commit

---
+++ IT/鸟哥的Linux私房菜基础篇.mm
@@ -0,0 +1,107 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1540171281740" ID="ID_816685894" MODIFIED="1540171329692" TEXT="&#x9e1f;&#x54e5;&#x7684;Linux&#x79c1;&#x623f;&#x83dc;&#x57fa;&#x7840;&#x7bc7;">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1540171361382" ID="ID_1751877102" MODIFIED="1540171378193" POSITION="right" TEXT="&#x7b2c;&#x96f6;&#x7ae0; &#x8ba1;&#x7b97;&#x5668;&#x6982;&#x8bba;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1540171388665" ID="ID_1255490066" MODIFIED="1540171409315" POSITION="right" TEXT="&#x7b2c;&#x4e00;&#x7ae0; Linux&#x662f;&#x4ec0;&#x4e48;&#x4e0e;&#x5982;&#x4f55;&#x5b66;&#x4e60;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1540171419512" ID="ID_1492484556" MODIFIED="1540171436595" POSITION="right" TEXT="&#x7b2c;&#x4e8c;&#x7ae0; &#x4e3b;&#x673a;&#x89c4;&#x5212;&#x4e0e;&#x78c1;&#x76d8;&#x5206;&#x533a;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1540171438232" ID="ID_1470994474" MODIFIED="1540171459794" POSITION="right" TEXT="&#x7b2c;&#x4e09;&#x7ae0; &#x5b89;&#x88c5;CentOS7.x">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1540171463049" ID="ID_1198122547" MODIFIED="1540171491413" POSITION="right" TEXT="&#x7b2c;&#x56db;&#x7ae0; &#x9996;&#x6b21;&#x767b;&#x5165;&#x4e0e;&#x5728;&#x7ebf;&#x6c42;&#x52a9;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1540171494109" ID="ID_1023403098" MODIFIED="1540171516631" POSITION="right" TEXT="&#x7b2c;&#x4e94;&#x7ae0; Linux&#x7684;&#x6587;&#x4ef6;&#x6743;&#x9650;&#x4e0e;&#x76ee;&#x5f55;&#x914d;&#x7f6e;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1540171544948" ID="ID_525273248" MODIFIED="1540171565915" POSITION="right" TEXT="&#x7b2c;&#x516d;&#x7ae0; Linux&#x6587;&#x4ef6;&#x4e0e;&#x76ee;&#x5f55;&#x7ba1;&#x7406;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1540171574715" ID="ID_1477458895" MODIFIED="1540171608002" POSITION="right" TEXT="&#x7b2c;&#x4e03;&#x7ae0; Linux&#x78c1;&#x76d8;&#x4e0e;&#x6587;&#x4ef6;&#x7cfb;&#x7edf;&#x7ba1;&#x7406;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1540171636618" ID="ID_1907056662" MODIFIED="1540171663356" POSITION="right" TEXT="&#x7b2c;&#x516b;&#x7ae0; &#x6587;&#x4ef6;&#x4e0e;&#x6587;&#x4ef6;&#x7cfb;&#x7edf;&#x7684;&#x538b;&#x7f29;&#x3001;&#x6253;&#x5305;&#x4e0e;&#x5907;&#x4efd;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1540171673147" ID="ID_226504989" MODIFIED="1540171686838" POSITION="right" TEXT="&#x7b2c;&#x4e5d;&#x7ae0; vim&#x7a0b;&#x5e8f;&#x7f16;&#x8f91;&#x5668;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1540171690577" ID="ID_674433077" MODIFIED="1540171711884" POSITION="right" TEXT="&#x7b2c;&#x5341;&#x7ae0; &#x8ba4;&#x8bc6;&#x4e0e;&#x5b66;&#x4e60;BASH">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1540171713892" ID="ID_613514632" MODIFIED="1540171753993" POSITION="right" TEXT="&#x7b2c;&#x5341;&#x4e00;&#x7ae0; &#x6b63;&#x89c4;&#x8868;&#x793a;&#x6cd5;&#x4e0e;&#x6587;&#x4ef6;&#x683c;&#x5f0f;&#x5316;&#x5904;&#x7406;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1540171770944" ID="ID_1314886491" MODIFIED="1540171787533" POSITION="right" TEXT="&#x7b2c;&#x5341;&#x4e8c;&#x7ae0; &#x5b66;&#x4e60;Shell Scripts">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1540171789828" ID="ID_961567288" MODIFIED="1540171819371" POSITION="right" TEXT="&#x7b2c;&#x5341;&#x4e09;&#x7ae0; Linux&#x8d26;&#x53f7;&#x7ba1;&#x7406;&#x4e0e;ACL&#x6743;&#x9650;&#x8bbe;&#x5b9a;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1540171874179" ID="ID_1248359069" MODIFIED="1540171924357" POSITION="right" TEXT="&#x7b2c;&#x5341;&#x56db;&#x7ae0; &#x78c1;&#x76d8;&#x914d;&#x989d;(Quota)&#x4e0e;&#x8fdb;&#x9636;&#x6587;&#x4ef6;&#x7cfb;&#x7edf;&#x7ba1;&#x7406;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1540171926242" ID="ID_711149704" MODIFIED="1540172005678" POSITION="right" TEXT="&#x7b2c;&#x5341;&#x4e94;&#x7ae0; &#x4f8b;&#x884c;&#x6027;&#x5de5;&#x4f5c;&#x6392;&#x7a0b;(crontab)">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1540172017595" ID="ID_628675823" MODIFIED="1540172060020" POSITION="right" TEXT="&#x7b2c;&#x5341;&#x516d;&#x7ae0; &#x8fdb;&#x7a0b;&#x7ba1;&#x7406;&#x4e0e;SELinux&#x521d;&#x63a2;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1540172073505" ID="ID_463439563" MODIFIED="1540172095750" POSITION="right" TEXT="&#x7b2c;&#x5341;&#x4e03;&#x7ae0; &#x8ba4;&#x8bc6;&#x7cfb;&#x7edf;&#x670d;&#x52a1;(daemons)">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1540172097138" ID="ID_1936754409" MODIFIED="1540172118475" POSITION="right" TEXT="&#x7b2c;&#x5341;&#x516b;&#x7ae0; &#x8ba4;&#x8bc6;&#x4e0e;&#x5206;&#x6790;&#x767b;&#x9646;&#x6863;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1540172157032" ID="ID_203916486" MODIFIED="1540172182654" POSITION="right" TEXT="&#x7b2c;&#x5341;&#x4e5d;&#x7ae0; &#x5f00;&#x673a;&#x6d41;&#x7a0b;&#x3001;&#x6a21;&#x5757;&#x7ba1;&#x7406;&#x4e0e;loader">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1540172199559" ID="ID_1086778210" MODIFIED="1540172221079" POSITION="right" TEXT="&#x7b2c;&#x4e8c;&#x5341;&#x7ae0; &#x57fa;&#x7840;&#x7cfb;&#x7edf;&#x8bbe;&#x5b9a;&#x4e0e;&#x5907;&#x4efd;&#x7b56;&#x7565;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1540172225140" ID="ID_399278659" MODIFIED="1540172266940" POSITION="right" TEXT="&#x7b2c;&#x4e8c;&#x5341;&#x4e00;&#x7ae0; &#x8f6f;&#x4ef6;&#x5b89;&#x88c5;&#xff1a;&#x539f;&#x59cb;&#x7801;&#x4e0e;Tarball">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1540172267770" ID="ID_886229165" MODIFIED="1540172311875" POSITION="right" TEXT="&#x7b2c;&#x4e8c;&#x5341;&#x4e8c;&#x7ae0; &#x8f6f;&#x4ef6;&#x5b89;&#x88c5; RPM&#x3001;SRPM&#x4e0e;YUM">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1540172312809" ID="ID_127433215" MODIFIED="1540172335277" POSITION="right" TEXT="&#x7b2c;&#x4e8c;&#x5341;&#x4e09;&#x7ae0; X Window&#x8bbe;&#x5b9a;&#x4ecb;&#x7ecd;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1540172373268" ID="ID_314467497" MODIFIED="1540172392325" POSITION="right" TEXT="&#x7b2c;&#x4e8c;&#x5341;&#x56db;&#x7ae0; Linux&#x6838;&#x5fc3;&#x7f16;&#x8bd1;&#x4e0e;&#x7ba1;&#x7406;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</map>
