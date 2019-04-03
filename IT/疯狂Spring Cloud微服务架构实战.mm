0adbeff6c96eb0fba15770a27eacd9200b406d8c
niuzhihuan <zhihuan.niu@funi.com>
2019-04-03 21:39:40 +0800

    commit

---
+++ IT/疯狂Spring Cloud微服务架构实战.mm
@@ -0,0 +1,107 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1554280947537" ID="ID_1002169456" MODIFIED="1554295306335" TEXT="&#x75af;&#x72c2;Spring Cloud&#x5fae;&#x670d;&#x52a1;&#x67b6;&#x6784;&#x5b9e;&#x6218;">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1554281017033" FOLDED="true" ID="ID_79239723" MODIFIED="1554282856191" POSITION="right" TEXT="&#x7b2c;1&#x7ae0; Spring Cloud&#x6982;&#x8ff0;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1554281065662" ID="ID_669445865" MODIFIED="1554281180035" TEXT="1.1 &#x4f20;&#x7edf;&#x7684;&#x5e94;&#x7528;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1554281074816" ID="ID_265695752" MODIFIED="1554281180038" TEXT="1.1.1 &#x5355;&#x4f53;&#x5e94;&#x7528;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1554281158003" ID="ID_1302674260" MODIFIED="1554281180043" TEXT="1.1.2 &#x67b6;&#x6784;&#x6f14;&#x8fdb;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1554281343706" ID="ID_1688328276" MODIFIED="1554281348722" TEXT="1.1.3 &#x67b6;&#x6784;&#x8981;&#x6c42;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1554281587049" ID="ID_1927896978" MODIFIED="1554281598207" TEXT="1.2 &#x5fae;&#x670d;&#x52a1;&#x4e0e;Spring Cloud">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1554281600086" ID="ID_1451312249" MODIFIED="1554281612381" TEXT="1.2.1 &#x4ec0;&#x4e48;&#x662f;&#x5fae;&#x670d;&#x52a1;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1554281743009" ID="ID_202168418" MODIFIED="1554281754113" TEXT="1.2.2 &#x5173;&#x4e8e;Netflix OSS">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1554281829290" ID="ID_522150640" MODIFIED="1554281842032" TEXT="1.2.3 Spring Cloud&#x4e0e;Netflix">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1554282028916" ID="ID_460527497" MODIFIED="1554282041175" TEXT="1.2.4 Spring Cloud&#x7684;&#x4e3b;&#x8981;&#x6a21;&#x5757;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1554282664841" ID="ID_1905771346" MODIFIED="1554282680029" TEXT="1.3 &#x5b66;&#x4e60;&#x65b9;&#x9762;&#x7684;&#x51c6;&#x5907;&#x5de5;&#x4f5c;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1554282681905" ID="ID_1845803475" MODIFIED="1554282707212" TEXT="1.3.1 &#x4e0b;&#x8f7d;&#x672c;&#x4e66;&#x7684;&#x8f6f;&#x4ef6;&#x53ca;&#x6e90;&#x7801;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1554282739300" ID="ID_1642598269" MODIFIED="1554282750222" TEXT="1.3.2 &#x5bfc;&#x5165;&#x672c;&#x4e66;&#x7684;&#x6848;&#x4f8b;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1554282845375" ID="ID_40029950" MODIFIED="1554282852652" TEXT="1.4 &#x672c;&#x7ae0;&#x5c0f;&#x7ed3;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1554282908805" ID="ID_1190897050" MODIFIED="1554282916259" POSITION="right" TEXT="&#x7b2c;2&#x7ae0; &#x642d;&#x5efa;&#x5f00;&#x53d1;&#x73af;&#x5883;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1554283205602" ID="ID_69863949" MODIFIED="1554283216518" TEXT="2.1 &#x5b89;&#x88c5;&#x4e0e;&#x914d;&#x7f6e;Maven">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1554283218088" ID="ID_522806188" MODIFIED="1554283227936" TEXT="2.1.1 &#x5173;&#x4e8e;Maven">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1554283339573" ID="ID_1201050306" MODIFIED="1554283351057" TEXT="2.1.2 &#x4e0b;&#x8f7d;&#x4e0e;&#x5b89;&#x88c5;Maven">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1554283426545" ID="ID_734183195" MODIFIED="1554284401137" TEXT="2.1.3 &#x914d;&#x7f6e;&#x8fdc;&#x7a0b;&#x4ed3;&#x5e93;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1554294717501" ID="ID_1459707942" MODIFIED="1554294793046" TEXT="2.2 &#x5b89;&#x88c5;eclipse">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1554294872872" ID="ID_1209853621" MODIFIED="1554294899969" TEXT="2.2.1 Eclipse&#x7248;&#x672c;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1554294939217" ID="ID_1740616503" MODIFIED="1554294941933" TEXT="2.2.2 &#x5728;Eclipse&#x4e2d;&#x914d;&#x7f6e;Maven">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1554295630804" ID="ID_1259530066" MODIFIED="1554295634916" TEXT="2.3 Spring Boot">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1554295649147" ID="ID_1069673816" MODIFIED="1554296644453" TEXT="2.3.1 Spring Boot&#x7b80;&#x4ecb;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1554296346581" ID="ID_1774390273" MODIFIED="1554296666804" TEXT="2.3.2 &#x65b0;&#x5efa;Maven&#x9879;&#x76ee;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1554296837430" ID="ID_425749229" MODIFIED="1554296854645" TEXT="2.3.3 &#x7f16;&#x5199;&#x542f;&#x52a8;&#x7c7b;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1554297231316" ID="ID_244101262" MODIFIED="1554297232723" TEXT="2.3.4 &#x7f16;&#x5199;&#x63a7;&#x5236;&#x5668;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1554298259269" ID="ID_830311242" MODIFIED="1554298260606" TEXT="2.3.5 &#x53d1;&#x5e03;RestWebService">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1554298279258" ID="ID_794498763" MODIFIED="1554298280281" TEXT="2.4 Spring Boot&#x914d;&#x7f6e;&#x6587;&#x4ef6;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1554298757728" ID="ID_86119470" MODIFIED="1554298758651" TEXT="2.4.1 &#x9ed8;&#x8ba4;&#x914d;&#x7f6e;&#x6587;&#x4ef6;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
---
+++ IT/软件设计师教程.mm
@@ -0,0 +1,55 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1554195557195" ID="ID_1797926008" MODIFIED="1554195739833" TEXT="&#x8f6f;&#x4ef6;&#x8bbe;&#x8ba1;&#x5e08;&#x6559;&#x7a0b;">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1554195619457" ID="ID_1542735298" MODIFIED="1554195739827" POSITION="right" TEXT="&#x7b2c;1&#x7ae0; &#x8ba1;&#x7b97;&#x673a;&#x7f51;&#x7edc;&#x6982;&#x8bba;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1554195682612" ID="ID_583860187" MODIFIED="1554195739833" POSITION="right" TEXT="&#x7b2c;2&#x7ae0; &#x7a0b;&#x5e8f;&#x8bbe;&#x8ba1;&#x8bed;&#x8a00;&#x57fa;&#x7840;&#x77e5;&#x8bc6;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1554195752392" ID="ID_1032062708" MODIFIED="1554195759789" POSITION="right" TEXT="&#x7b2c;3&#x7ae0; &#x6570;&#x636e;&#x7ed3;&#x6784;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1554196167707" ID="ID_795151073" MODIFIED="1554196176715" POSITION="right" TEXT="&#x7b2c;4&#x7ae0; &#x64cd;&#x4f5c;&#x7cfb;&#x7edf;&#x77e5;&#x8bc6;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1554196185799" ID="ID_1143525339" MODIFIED="1554196198727" POSITION="right" TEXT="&#x7b2c;5&#x7ae0; &#x8f6f;&#x4ef6;&#x5de5;&#x7a0b;&#x57fa;&#x7840;&#x77e5;&#x8bc6;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1554196240423" ID="ID_755235241" MODIFIED="1554196252864" POSITION="right" TEXT="&#x7b2c;6&#x7ae0; &#x7ed3;&#x6784;&#x5316;&#x5f00;&#x53d1;&#x65b9;&#x6cd5;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1554196418846" ID="ID_1181980396" MODIFIED="1554196426835" POSITION="right" TEXT="&#x7b2c;7&#x7ae0; &#x9762;&#x5411;&#x5bf9;&#x8c61;&#x6280;&#x672f;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1554196429239" ID="ID_1862662882" MODIFIED="1554196441963" POSITION="right" TEXT="&#x7b2c;8&#x7ae0; &#x7b97;&#x6cd5;&#x8bbe;&#x8ba1;&#x4e0e;&#x5206;&#x6790;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1554196486346" ID="ID_1252274342" MODIFIED="1554196498723" POSITION="right" TEXT="&#x7b2c;9&#x7ae0; &#x6570;&#x636e;&#x5e93;&#x6280;&#x672f;&#x57fa;&#x7840;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1554196500828" ID="ID_42555707" MODIFIED="1554196519026" POSITION="right" TEXT="&#x7b2c;10&#x7ae0; &#x7f51;&#x7edc;&#x4e0e;&#x4fe1;&#x606f;&#x5b89;&#x5168;&#x57fa;&#x7840;&#x77e5;&#x8bc6;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1554196526962" ID="ID_1751747600" MODIFIED="1554196557875" POSITION="right" TEXT="&#x7b2c;11&#x7ae0; &#x6807;&#x51c6;&#x5316;&#x548c;&#x8f6f;&#x4ef6;&#x77e5;&#x8bc6;&#x4ea7;&#x6743;&#x57fa;&#x7840;&#x77e5;&#x8bc6;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1554196566107" ID="ID_386432894" MODIFIED="1554196577060" POSITION="right" TEXT="&#x7b2c;12&#x7ae0; &#x8f6f;&#x4ef6;&#x7cfb;&#x7edf;&#x5206;&#x6790;&#x4e0e;&#x8bbe;&#x8ba1;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</map>
